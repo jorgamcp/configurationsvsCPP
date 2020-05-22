93bdba0cf5d4b81a8132f9827323c87c1743a459
Jorge <jorgam31@gmail.com>
2020-05-22 15:16:27 +0200

    Remove test function. Nothing else.

--- hola.cpp
+++ hola.cpp
@@ -2,15 +2,10 @@
 
 using namespace std;
 
-int mimodificacion(int i,int k)
-{
-  return i + k;
-}
-
+ 
 int main()
 {
   cout << "Hola Mundo" << endl;
-  mimodificacion(29,94);
   getchar(); // para pausar la consola de windows.
   return 0;
 }
